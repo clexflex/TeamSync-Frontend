02831ca986445296ba09a998197f5d2f6b133243
yashraj <yashrajjghosalkar@gmail.com>
2025-01-08 13:18:36 +0530

    necessary

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    API_URL: import.meta.env.VITE_API_URL || "https://team-sync-serverb1.vercel.app",
 };
 
 export default config;
