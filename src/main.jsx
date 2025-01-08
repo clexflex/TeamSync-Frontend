a44e1693b1bd97c09ce1b86a5290a5cfa68c3e5b
yashraj <yashrajjghosalkar@gmail.com>
2025-01-08 12:47:52 +0530

    Necessary

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AuthContext from './context/authContext.jsx'
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContext>
       <App />
+      <SpeedInsights />
     </AuthContext>
   </StrictMode>,
 )
